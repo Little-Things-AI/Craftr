4792fe6fa2dbe334d8a0b81e8544214b49982971
Ryan Hartman <73214283+ryanhartman4@users.noreply.github.com>
2023-01-04 07:59:45 +0000

    updated tailwind.config.js with new directory. Changed hidden .replit file to run "npm run dev" instead of "npm run start"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./use_cases/Starfish/pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
