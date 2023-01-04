99378e1386258e3b686ff768631a351f94fcd30c
Little-Things-Co <121212538+Little-Things-Co@users.noreply.github.com>
2023-01-03 22:52:56 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,15 @@ import {
   Bars3Icon, 
   XMarkIcon
 } from "@heroicons/react/solid";
-
 export default function Home() {
   // React Hooks
   const [data, setData] = useState({ text: "" });
   const [query, setQuery] = useState();
   const [query2, setQuery2] = useState();
+  const [query3, setQuery3] = useState();
   const [search, setSearch] = useState();
   const [search2, setSearch2] = useState();
+  const [search3, setSearch3] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
   // Fetch data from API
@@ -31,6 +32,7 @@ export default function Home() {
           body: JSON.stringify({
             name: search,
             name2: search2,
+            name3: search3,
           }),
           headers: {
             "Content-Type": "application/json",
@@ -44,60 +46,73 @@ export default function Home() {
     };
 
     fetchData();
-  }, [search, search2]);
-  
+  }, [search, search2, search3]);
   // What we want to render
   return (
     <Fragment>
       <Head>
-        <title>What's Next?</title>
+        <title>Self-Prop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="bg-gradient-to-tr from-pink-200 to-blue-400 min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+      <div className="bg-gradient-to-br from-emerald-400 via-blue-600 to-violet-500 min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
         <main className="flex flex-col justify-center  max-w-3xl w-full align-center">
-          <h1 className="text-4xl text-center font-bold text-slate-800 drop-shadow sm:text-2xl mb-1">
-            What's Next?
+          <h1 className="transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:text-indigo-600 duration-300 text-4xl text-center font-semibold text-slate-800 drop-shadow sm:text-2xl mb-1 mx-16">
+            Starfish
           </h1>
-          <div className="relative overflow-hidden bg-gradient-to-br from-indigo-300 to-red-300 rounded-md py-1.5 px-4 text-sm leading-6 ring-4 mt-4 ring-gray-900/10 hover:ring-gray-900/20">
-          <p className="block text-sm text-center mt-4 text-gray-500">
-            What do you want to do?
-          </p>
+          <div className="relative overflow-hidden bg-gradient-to-bl from-emerald-400 via-blue-300 to-violet-400 rounded-3xl py-2 px-8 text-sm leading-6 ring-4 mt-6 ring-gray-900/10 hover:ring-gray-900/20">
+            <p className="block text-[16px] text-center mt-6 text-gray-800 underline decoration-violet-500">
+            Select a topic or domain.
+        </p>
 
           {/* Card & Input field  */}
-          <div className="text-center relative backdrop-filter overflow-hidden mb-6 max-w w-full rounded-md  ring-1 ring-black ring-opacity-0 p-4 ">
+          <div className="text-center relative backdrop-filter overflow-hidden mb-6 max-w w-full rounded-md  ring-1 ring-black ring-opacity-0 px-4 py-2 ">
             <textarea
-              className="max-w shadow-md bg-gray-100 text-gray-800 px-4 py-2   min-h-64  block w-full focus:ring-pink-500 focus:border-pink-500 sm:text-sm border border-gray-300 rounded-lg"
+              className="max-w shadow-md bg-gray-100 text-gray-800 px-4 py-2   h-24  block w-full caret-pink-500 focus:ring-pink-500 focus:border-pink-500 text-sm border border-gray-300 rounded-lg"
               type="textarea"
               value={query}
               onChange={(event) => setQuery(event.target.value)}
-              placeholder="(e.g. expand music reach, promote my business, stop a bad habit, learn an instrument)"
+              placeholder="(Natural language processing, language translation, sentiment analysis)"
               defaultValue={""}
             />
 
             
-            <p className="block text-sm text-center mt-4 text-gray-500">
-            What is your intended goal?
+            <p className="block text-[16px] text-center mt-8 px-4 text-gray-800 underline decoration-sky-500">
+            Enter a specific task for your prompt.
           </p>
 
           {/* Card & Input field  */}
-          <div className="text-center relative backdrop-filter overflow-hidden mb-6 mt-4 max-w w-full rounded-md  ring-1 ring-black ring-opacity-0 p-0 ">
+          <div className="text-center relative backdrop-filter overflow-hidden mb-6  px-0 max-w w-full rounded-md  ring-1 ring-black ring-opacity-0 p-2 ">
             <textarea
-              className="max-w shadow-md bg-gray-100 text-gray-800 px-4 py-2    min-h-64  block w-full focus:ring-pink-500 focus:border-pink-500 sm:text-sm border border-gray-300 rounded-lg"
+              className="max-w shadow-md bg-gray-100 text-gray-800 px-4 py-2 h-24 block w-full caret-pink-500 focus:ring-inset focus:ring-pink-500 focus:border-pink-500 text-sm border border-gray-300 rounded-lg"
               type="textarea"
               value={query2}
               onChange={(event) => setQuery2(event.target.value)}
-              placeholder="Write a specific goal here"
+              placeholder="Describe your task (e.g. write a summary of the main points in this article, analyze and shift the tone of a passage)"
+              defaultValue={""}
+            />
+            <p className="block text-[16px] text-center mt-8 px-8 text-gray-800 underline decoration-violet-500">
+            (Optional) Any additional details or requirements for your prompt.
+          </p>
+
+          {/* Card & Input field  */}
+          <div className="text-center relative backdrop-filter overflow-hidden mb-6 max-w w-full rounded-md  ring-1 ring-black ring-opacity-0 py-2 ">
+            <textarea
+              className="max-w shadow-md bg-gray-100 text-gray-800 px-4 py-2    h-32  block w-full caret-pink-500 focus:ring-inset focus:ring-pink-500 focus:border-pink-500 text-sm border border-gray-300 rounded-lg"
+              type="textarea"
+              value={query3}
+              onChange={(event) => setQuery3(event.target.value)}
+              placeholder="(e.g. the summary should be concise and focus on the main points of the article, the sentiment of the text should be accurately classified)"
               defaultValue={""}
             />
             
             {/* Button to that calls API */}
-            <div className="text-center relative backdrop-filter overflow-hidden mb-6 max-w w-full rounded-md  ring-1 ring-black ring-opacity-0 py-4 ">  
+            <div className="text-center relative backdrop-filter overflow-hidden mb-6 mt-2 max-w w-full rounded-md  ring-1 ring-black ring-opacity-0 py-4 ">  
             <button
-              className="inline-block rounded-lg bg-blue-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-blue-600 hover:bg-blue-700 hover:ring-blue-700"
+              className="inline-block rounded-full bg-gradient-to-tr from-emerald-500/90 via-blue-600/80 to-violet-500 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-2 ring-emerald-600 hover:from-pink-500 hover:to-yellow-500 hover:ring-teal-600"
               type="button"
-              onClick={() => [setSearch(query), setSearch2(query2)]}
+              onClick={() => [setSearch(query), setSearch2(query2), setSearch3(query3)]}
             >
-              Result
+              Prompt Me!
             </button>
 
               <div className="mt-5 p-5 text-sm text-gray-900 border-t-2 border-slate-200 ">
@@ -106,6 +121,7 @@ export default function Home() {
               </div>
             </div>
           </div>
+           </div> 
             </div>
           </div>  
         </main>
